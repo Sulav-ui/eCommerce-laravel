f7b03daf2f2552a83d8bc3beabd3ef5a376b9ce1
Sulav <ksulav10@gmail.com>
2022-07-13 14:33:38 +0545

    header,footer and login page done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="bg-light text-center text-lg-start">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2020 Copyright:
+        <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.1);">
+        © 2020 Copyright:
+        <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+</footer>
